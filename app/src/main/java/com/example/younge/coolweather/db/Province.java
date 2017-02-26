8cef0e2a38ee3750de5eb93a368c011ce3ac5d90
Your Name <you@example.com>
2017-02-26 17:21:20 +0800

    加入创建数据库和表的各的各项配

---
+++ app/src/main/java/com/example/younge/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.younge.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by younge on 2017/2/26 0026.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
